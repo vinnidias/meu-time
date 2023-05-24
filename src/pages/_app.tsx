330a6d86d17068997f244501cc2b6109a5763210
vinnidias <vinicius.3991@gmail.com>
2023-05-24 20:27:39 -0300

    remove AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />;
 }
