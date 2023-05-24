fe9ff1d94573af6ced32eabc0f62a7584fcc8ddb
vinnidias <vinicius.3991@gmail.com>
2023-05-24 20:27:25 -0300

    unusless space

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const api = axios.create({baseURL: "https://v3.football.api-sports.io"});
+export const api = axios.create({
+  baseURL: "https://v3.football.api-sports.io",
+});
