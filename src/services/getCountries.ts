5cb4a0496cc0d85a0acce4e10f557ec4df81666d
vinnidias <vinicius.3991@gmail.com>
2023-05-24 00:25:51 -0300

    get countrie route created

---
+++ src/services/getCountries.ts
@@ -0,0 +1,25 @@
+import { ICountriesProps } from "@/interfaces/ICountriesProps";
+import { api } from "./api";
+
+
+export const getCountries = async (
+  apiKey: string
+): Promise<{
+  data: ICountriesProps;
+  status: number;
+  errors: { token: string };
+}> => {
+  const config = {
+    headers: {
+      "x-rapidapi-host": "v3.football.api-sports.io",
+      "x-rapidapi-key": apiKey,
+    },
+  };
+
+  const res = await api.get("/countries", config);
+  const data = res.data.response;
+  const errors = res.data.errors;
+  const status = res.data.status;
+
+  return { data, errors, status };
+};
