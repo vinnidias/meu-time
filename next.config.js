7e732124e8cfbfc8327a6338d1764a2cc9396521
vinnidias <vinicius.3991@gmail.com>
2023-05-26 12:08:08 -0300

    media paths config to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,53 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "media-1.api-sports.io",
+      "media-2.api-sports.io",
+      "media-3.api-sports.io",
+      "media-4.api-sports.io",
+      "media-5.api-sports.io",
+      "media-6.api-sports.io",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media-1.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-2.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-3.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-4.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-5.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media-6.api-sports.io",
+        port: "",
+        pathname: "/football/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
