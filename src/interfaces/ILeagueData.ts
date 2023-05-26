76212820bed01b890ca929a0ed8a4900e20fdc03
vinnidias <vinicius.3991@gmail.com>
2023-05-26 19:47:02 -0300

    interfaces to league data and response

---
+++ src/interfaces/ILeagueData.ts
@@ -0,0 +1,35 @@
+export interface ILeagueData {
+  league: {
+    id: number;
+    name: string;
+    type: string;
+    logo: string;
+  };
+  country: {
+    name: string;
+    code: string | null;
+    flag: string | null;
+  };
+  seasons: {
+    year: number;
+    start: string;
+    end: string;
+    current: boolean;
+    coverage: {
+      fixtures: {
+        events: boolean;
+        lineups: boolean;
+        statistics_fixtures: boolean;
+        statistics_players: boolean;
+      };
+      standings: boolean;
+      players: boolean;
+      top_scorers: boolean;
+      top_assists: boolean;
+      top_cards: boolean;
+      injuries: boolean;
+      predictions: boolean;
+      odds: boolean;
+    };
+  }[];
+}
