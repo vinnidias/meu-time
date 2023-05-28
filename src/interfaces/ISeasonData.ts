731385d10eeed32dafbd2fc30e241046c6ffafb6
vinnidias <vinicius.3991@gmail.com>
2023-05-27 22:02:23 -0300

    season interface created

---
+++ src/interfaces/ISeasonData.ts
@@ -0,0 +1,22 @@
+export interface ISeasonData {
+  year: number;
+  start: string;
+  end: string;
+  current: boolean;
+  coverage: {
+    fixtures: {
+      events: boolean;
+      lineups: boolean;
+      statistics_fixtures: boolean;
+      statistics_players: boolean;
+    };
+    standings: boolean;
+    players: boolean;
+    top_scorers: boolean;
+    top_assists: boolean;
+    top_cards: boolean;
+    injuries: boolean;
+    predictions: boolean;
+    odds: boolean;
+  };
+}
