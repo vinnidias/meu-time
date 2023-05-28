8b3038bfbcd96446cc19923325d86754381c2a93
vinnidias <vinicius.3991@gmail.com>
2023-05-27 22:02:01 -0300

    season state created

--- src/hooks/useLeagues.tsx
+++ src/hooks/useLeagues.tsx
@@ -3,12 +3,15 @@ import { useRouter } from "next/router";
 import Cookies from "js-cookie";
 import { getLeagueByCountry } from "@/services/getLeagueByCountry";
 import { ILeagueData } from "@/interfaces/ILeagueData";
+import { ISeasonData } from "@/interfaces/ISeasonData";
 
 export const useLeagues = () => {
   const router = useRouter();
   const [countrySelected, setCountrySelected] = useState("");
   const [leagueByCountry, setLeagueByCountry] = useState<ILeagueData[]>([]);
   const [leagueSelect, setLeagueSelect] = useState("");
+  const [seasons, setSeasons] = useState<ISeasonData[]>([]);
+  const [selectedSeason, setSelectedSeason] = useState(0);
 
   useEffect(() => {
     const apiKey = Cookies.get("api_key") || "";
@@ -29,11 +32,20 @@ export const useLeagues = () => {
     })();
   }, [countrySelected, router]);
 
+  useEffect(() => {
+    const seasons = leagueByCountry.map((league) => league.seasons);
+    const formatedSeasons = seasons[0];
+    setSeasons(formatedSeasons);
+  }, [leagueByCountry]);
+
   return {
     countrySelected,
     setCountrySelected,
     leagueByCountry,
     leagueSelect,
     setLeagueSelect,
+    seasons,
+    selectedSeason,
+    setSelectedSeason,
   };
 };
